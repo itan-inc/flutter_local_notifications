4b8f732667dc782a1bbcd25930eb16ae85fc7b4a
sun tianhai <tenyears13700114938@gmail.com>
2022-05-10 20:23:08 +0900

    add the number of items notification represents for android

--- flutter_local_notifications/lib/src/platform_specifics/android/notification_details.dart
+++ flutter_local_notifications/lib/src/platform_specifics/android/notification_details.dart
@@ -52,6 +52,7 @@ class AndroidNotificationDetails {
     this.subText,
     this.tag,
     this.colorized = false,
+    this.number,
   });
 
   /// The icon that should be used when displaying the notification.
@@ -291,4 +292,7 @@ class AndroidNotificationDetails {
   /// For most styles, the colouring will only be applied if the notification is
   /// for a foreground service notification.
   final bool colorized;
+
+  /// Sets the number of items this notification represents. May be displayed as a badge count for Launchers that support badging.
+  final int? number;
 }
